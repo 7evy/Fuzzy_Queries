f6469a37d9f5ba7eaf0b3a44121eaf8a0d17a222
Alzaphano <mlaumonn@enssat.fr>
2020-11-27 14:34:02 +0100

    Interface web V0.1

--- web_app/fuzzy_queries/views.py
+++ web_app/fuzzy_queries/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from fuzzy_queries.models import Immo
 
 # Create your views here.
+
+def index(request):
+    immo = Immo.objects.all()
+    context = {
+        'immo': immo
+    }
+    return render(request, 'fuzzy_queries/index.html', context)
