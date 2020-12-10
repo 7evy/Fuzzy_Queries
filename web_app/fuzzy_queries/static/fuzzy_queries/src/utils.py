11fc931efbbd8a344ca5697388100bfd52808481
Alzaphano <mlaumonn@enssat.fr>
2020-12-10 10:16:02 +0100

    supp import dataset in utils

--- web_app/fuzzy_queries/static/fuzzy_queries/src/utils.py
+++ web_app/fuzzy_queries/static/fuzzy_queries/src/utils.py
@@ -1,9 +1,6 @@
 from operator import *
 import numpy as np
-try :
-    import fuzzy_queries.static.fuzzy_queries.src.dataset as ds
-except :
-    import dataset as ds
+
 
 def discrete_distance(x, y):
     """Distance between small integers."""
@@ -28,11 +25,3 @@ def relative_sim(x, y):
 def inf_or_relative(x, y):
     """Useful if x < y is as good as x == y."""
     return 1 if x <= y else relative_sim(x, y)
-
-F = [eq, eq, relative_sim, discrete_sim, discrete_sim, inf_or_relative, eq, eq, eq, relative_sim, relative_sim, relative_sim]
-T = [0, 0, 0.75, 0.5, 0.5, 0.75, 0, 0, 0, 0.75, 0.75, 0.75]
-
-def random_selection(data, n):
-    sel = [data[k] for k in np.random.randint(0, len(data), n)]
-    D = ds.Dataset(sel, F, T)
-    return sel, D.select_most_satisfying(data, 20)
