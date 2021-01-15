a9d7b9b334c3a83de97b38c23242adb1d35c9725
Alzaphano <mlaumonn@enssat.fr>
2021-01-15 15:04:51 +0100

    Auto stash before merge of "main" and "origin/main"

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -76,12 +76,23 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'immo',
+#         'USER': 'root', 
+#         'PASSWORD': 'occulto',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'immo',
-        'USER': 'root', 
-        'PASSWORD': 'occulto',
+        'NAME': 'mydb',
+        'USER': 'mathieu', 
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '5432',
     }
