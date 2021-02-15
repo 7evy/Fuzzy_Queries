68a66c98ba82d177942d1ed20e450d14833cd6b2
Dejan PARIS <parisdejan@gmail.com>
2021-02-15 10:06:22 +0100

    hiding secret key

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from web_app.secret import *
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c$$g==h9wgagp3yw5hl(@c(t&3&5ha*2grrdl$v70@+)1r3d&1'
+SECRET_KEY = secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
